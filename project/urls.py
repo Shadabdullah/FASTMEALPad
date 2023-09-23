f9d76dcd639e43d23ad7654b3eed20ae8d8fb100
Shadabdullah <shadabdullah101@gmail.com>
2023-09-23 10:05:47 +0530

    Path changed:

--- project/urls.py
+++ project/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import include, path, re_path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('getmealfast-shad-admin/', admin.site.urls),
     path('', include("base.urls")),
    
 ]
