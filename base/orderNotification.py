9af76a34251e123c46ae4ea2a878303c73c617ac
Shad <97920809+Shadabdullah@users.noreply.github.com>
2023-11-15 15:26:37 +0530

    Update orderNotification.py

--- base/orderNotification.py
+++ base/orderNotification.py
@@ -3,8 +3,8 @@ from telegram import Bot
 
 async def send_welcome_message_async(user_name):
   
-    TELEGRAM_CHAT_ID = '-1001877500762'
-    BOT_TOKEN = '6591914802:AAGTWKCa4X90NPO_xO1OvcGC6M2pNy0Y0Ao'
+    TELEGRAM_CHAT_ID = '-1001877500'
+    BOT_TOKEN = '6591914802:'
     bot = Bot(token=BOT_TOKEN)
     message = "You have One New Order From "+ str(user_name)
    
